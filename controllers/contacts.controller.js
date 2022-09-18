a12d9d801ce9e5bb0569bbf29e2f118d86289dbb
jesse-wilson88 <jesse.emmert88@gmail.com>
2022-09-17 20:52:49 -0600

    I think I have everything done for this assignment

---
+++ controllers/contacts.controller.js
@@ -0,0 +1,5 @@
+const personsName = (req, res) => {
+  res.send("Barbara is my wife.");
+};
+
+module.exports = { contactName };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const routes = require("express").Router();
+const { contactName } = require("../controllers/contacts.controller.js");
 
-routes.get("/", (req, res) => {
-  res.send("Barbara Wilson");
-});
+// routes.get("/", (req, res) => {
+//   res.send("Barbara Wilson");
+// });
+
+routes.get("/", contactName);
 
 module.exports = routes;
