3bbe1f161adbf46a843305d4dd9ab0343b902195
jesse-wilson88 <jesse.emmert88@gmail.com>
2022-09-24 14:39:06 -0600

    SAeeing if I can get the same result on render

---
+++ controllers/contacts.js
@@ -0,0 +1,17 @@
+// const contactName = (req, res) => {
+//   res.send("Barbara Wilson is my wife.");
+// };
+
+// module.exports = { contactName };
+
+const mongodb = require("../db/connect");
+
+const getAll = async (req, res) => {
+  const result = await mongodb.getDb().db().collection("contacts").find();
+  result.toArray().then((lists) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(lists);
+  });
+};
+
+module.exports = { getAll };
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(
+      `Personal week 02: Connected to DB & listening on port ${port}`
+    );
+  }
+});
+
+// app.use("/", require("./routes"));
+
+// app.listen(port, () => {
+//   console.log(`Personal Week 02: Running on port ${port}.`);
+// }
