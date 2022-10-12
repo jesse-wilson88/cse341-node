632a3ae1a4ee8b6e944fb4d1e600e17bf49d79c6
jesse-wilson88 <jesse.emmert88@gmail.com>
2022-10-11 22:38:30 -0600

    Moved everything into pwk4 folder

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const openCors = require("../middleware/openCors");
-
-router.use(openCors);
-
-router.use("/contacts", require("./contacts"));
-router.use("/api-docs", require("./docs"));
-
-module.exports = router;
